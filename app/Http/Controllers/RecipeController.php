04bd50c911bdae5e529346cebce0319cfb983299
Zun-U <hello.tra.uenishi@gmail.com>
2022-08-05 15:48:06 +0900

    動的に追加された画像ファイルパスのDB登録

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,17 +8,15 @@ use App\Recipe;
 use App\Content;
 use App\Foodstuff;
 
+// Storageクラスの使用（画像保存の為）
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-
-
-
-
-
 
 
     // 全レシピの取得
@@ -48,23 +46,37 @@ class RecipeController extends Controller
 
         // test
         // ------------------------------
-        $explanations = $request->input('content');
+        // $explanations = $request->input('content');
 
-        dump($explanations);
+        // dump($explanations);
 
-        foreach ($explanations['text'] as $key => $value) {
+        // foreach ($explanations['text'] as $key => $value) {
 
-            dump($explanations['text'][$key]);
-            dump($explanations['image'][$key]);
-        }
-        exit;
+        //     dump($explanations['text'][$key]);
+        //     dump($explanations['image'][$key]);
+        // }
+        // exit;
         // ------------------------------
 
 
+        // TOPレシピ画像ファイルの取得
+        $recipe_image = $request->file('product_image');
+
+        // 画像ファイルがあれば
+        if ($recipe_image) {
+            $image_path = Storage::disk("public")->putFile('profile', $recipe_image);
+            $imagePath = "/storage/" . $image_path;
+            $recipe->product_image = $imagePath;
+        }
+        // 画像ファイルがなければ
+        else {
+            $image_path = null;
+        }
+
         // recipesテーブルに各値を登録
         $recipe->title = $request->title;
+        $recipe->subtitle = $request->subtitle;
         $recipe->howmany = $request->howmany;
-        $recipe->product_image = $request->product_image;
         $recipe->cooking_time = $request->cooking_time;
         $recipe->ages = $request->ages;
 
@@ -90,12 +102,29 @@ class RecipeController extends Controller
 
         // contentsテーブルに登録
         $explanations = $request->input('content');
+        $upload_image = $request->file('upload_image');
 
 
+
+        // 各入力欄の共通項である「key」を取得（連番を取得）
         foreach ($explanations['text'] as $key => $value) {
+
             $content = new Content();
+
+            // 画像ファイルの有無判定（keyに紐づいた入力値を判定）
+            if (isset($upload_image['cooking_image'][$key])) {
+                $uploard_path = Storage::disk("public")->putFile('profile', $upload_image['cooking_image'][$key]);
+                $uploadPath = "/storage/" . $uploard_path;
+                $content->recipe_image = $uploadPath;
+            } else {
+                // ファイルがなければNULLを返す。
+                $uploard_path = NULL;
+            }
+
+
+
+
             $content->content = $explanations['text'][$key];
-            $content->recipe_image = $explanations['image'][$key];
             $recipe->contents()->save($content);
         }
 
