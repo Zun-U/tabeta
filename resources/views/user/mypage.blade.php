b4fa2b63220ab613420ef9f7b7d97448e2bf8a1f
Zun-U <hello.tra.uenishi@gmail.com>
2022-08-25 10:37:15 +0900

    レイアウト調整

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -5,7 +5,6 @@
 
 <div class="container">
   <div class="row">
-<<<<<<< HEAD
     <div class="col-2">
       <div class="mt-5 font-weight">
         <p>プロフィール画像</p>
@@ -16,19 +15,6 @@
           <div class="edit-here">
             <!-- 三項演算子で画像が登録されていなければnoiconを乗せる。 -->
             <img src="{{ Auth::user()->image === null ? '/images/noimage.png' : Auth::user()->image }}" class="img-fluid circle rounded-3 prof-hover" width="200%" data-profile-id="{{ $mypages->id }}">
-=======
-
-    <div class="mt-5">
-      <p>プロフィール画像</p>
-    </div>
-
-    <div>
-      <form action="" method="POST" id="edit_image" enctype="multipart/form-data">
-        <label class="col-2">
-          <div class="edit-here">
-            <!-- 三項演算子で画像が登録されていなければnoiconを乗せる。 -->
-            <img src="{{ Auth::user()->image === null ? '/images/noimage.png' : Auth::user()->image }}" class="img-fluid circle rounded-3 prof-hover" data-profile-id="{{ $mypages->id }}">
->>>>>>> master
           </div>
           <div>
             <input type="file" name="profile_image" id="edit-profile" class="edit-image" value="" style="display:none" accept="image/*">
@@ -50,7 +36,7 @@
           @csrf
           <div class="mt-2">
             <label class="form-label font-weight" for="name">名前</label>
-            <input type="text"  name="name" class="form-control" id="edit-name" value="{{ Auth::user()->name }}">
+            <input type="text" name="name" class="form-control" id="edit-name" value="{{ Auth::user()->name }}">
           </div>
           <div class="mt-2">
             <label class="form-label font-weight" for="email">メールアドレス</label>
@@ -79,12 +65,6 @@
 <div class="container d-flex">
 
   @foreach($mypages->recipes->sortByDesc('created_at') as $recipe)
-
-<<<<<<< HEAD
-
-  <div class="card shadow rounded-3 me-3 card-move mb-5" style="width: 18rem;">
-=======
->>>>>>> master
 
   <div class="card shadow rounded-3 me-3 card-move mb-5" style="width: 18rem;">
     <div class="card-img-top">
@@ -139,10 +119,6 @@
         </div>
         @endif
       </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     </div>
   </div>
 
@@ -210,84 +186,74 @@
         </div>
         @endif
       </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     </div>
   </div>
   @endforeach
+</div>
+
+<div class="myarticle-group my-4 mt-5 container">
+  ブックマーク
+</div>
+
+<div class="container d-flex">
+  @foreach($mypages->recipes->sortByDesc('created_at') as $recipe)
+
+  <div class="card shadow rounded-3 me-3" style="width: 18rem;">
+    <div class="card-img-top">
+      <img src="{{ $recipe->product_image }}" width="100%">
+    </div>
+    <div class="card-body">
+      <div>
+        <a href="{{ route('article.detail', $recipe) }}" class="card-link stretched-link"></a>
+      </div>
+      <div class="card-text">{{ $recipe->created_at }}</div>
+      <div>
+        <h5 class="card-title">{{ $recipe->title }}</h2>
+      </div>
+      <div class="card-text">{{ $recipe->howmany }}人分</div>
+      <div class="card-text">調理時間：{{ $recipe->cooking_time }}分</div>
+      <div class="card-text">対象年齢：{{ $recipe->ages }}</div>
 
 
-<<<<<<< HEAD
+      <div class="d-flex justify-content-end">
+        <!-- いいね数表示 -->
+        @if (!$recipe->isLikedBy(Auth::user()))
+        <div class="me-3">
+          <span>
+            <i class="fa-solid fa-carrot"></i>
+            <span class="like-counter">{{ ($recipe->likes_count == 0) ? "" : $recipe->likes_count }}</span>
+          </span>
+        </div>
+        @else
+        <div class="me-3">
+          <span>
+            <i class="fa-solid fa-carrot liked">たべた！</i>
+            <span class="like-counter">{{ ($recipe->likes_count == 0) ? "" : $recipe->likes_count }}</span>
+          </span>
+        </div>
+        @endif
+
+
+        <!-- ブックマーク数表示 -->
+        @if (!$recipe->isMarkedBy(Auth::user()))
+        <div>
+          <span>
+            <i class="fa-solid fa-bookmark"></i>
+            <span class="bookmark-counter">{{ ($recipe->favorites_count == 0) ? "" : $recipe->favorites_count }}</span>
+          </span>
+        </div>
+        @else
+        <div>
+          <span>
+            <i class="fa-solid fa-bookmark marked"></i>
+            <span class="bookmark-counter">{{ ($recipe->favorites_count == 0) ? "" : $recipe->favorites_count }}</span>
+          </span>
+        </div>
+        @endif
+      </div>
+    </div>
+  </div>
+  @endforeach
 </div>
 
 @endsection
-=======
-  <div class="myarticle-group my-4 mt-5 container">
-    ブックマーク
-  </div>
-
-  <div class="container d-flex">
-    @foreach($mypages->recipes->sortByDesc('created_at') as $recipe)
-
-    <div class="card shadow rounded-3 me-3" style="width: 18rem;">
-      <div class="card-img-top">
-        <img src="{{ $recipe->product_image }}" width="100%">
-      </div>
-      <div class="card-body">
-        <div>
-          <a href="{{ route('article.detail', $recipe) }}" class="card-link stretched-link"></a>
-        </div>
-        <div class="card-text">{{ $recipe->created_at }}</div>
-        <div>
-          <h5 class="card-title">{{ $recipe->title }}</h2>
-        </div>
-        <div class="card-text">{{ $recipe->howmany }}人分</div>
-        <div class="card-text">調理時間：{{ $recipe->cooking_time }}分</div>
-        <div class="card-text">対象年齢：{{ $recipe->ages }}</div>
-
-
-        <div class="d-flex justify-content-end">
-          <!-- いいね数表示 -->
-          @if (!$recipe->isLikedBy(Auth::user()))
-          <div class="me-3">
-            <span>
-              <i class="fa-solid fa-carrot"></i>
-              <span class="like-counter">{{ ($recipe->likes_count == 0) ? "" : $recipe->likes_count }}</span>
-            </span>
-          </div>
-          @else
-          <div class="me-3">
-            <span>
-              <i class="fa-solid fa-carrot liked">たべた！</i>
-              <span class="like-counter">{{ ($recipe->likes_count == 0) ? "" : $recipe->likes_count }}</span>
-            </span>
-          </div>
-          @endif
-
-
-          <!-- ブックマーク数表示 -->
-          @if (!$recipe->isMarkedBy(Auth::user()))
-          <div>
-            <span>
-              <i class="fa-solid fa-bookmark"></i>
-              <span class="bookmark-counter">{{ ($recipe->favorites_count == 0) ? "" : $recipe->favorites_count }}</span>
-            </span>
-          </div>
-          @else
-          <div>
-            <span>
-              <i class="fa-solid fa-bookmark marked"></i>
-              <span class="bookmark-counter">{{ ($recipe->favorites_count == 0) ? "" : $recipe->favorites_count }}</span>
-            </span>
-          </div>
-          @endif
-        </div>
-      </div>
-    </div>
-    @endforeach
-  </div>
-
-  @endsection
->>>>>>> master
